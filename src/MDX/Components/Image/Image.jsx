4f6c942b1d02b0d3362152620a4a3830d017d43f
Paras Mogla <paras@getswan.co>
2024-12-24 16:48:27 +0530

    arguments and few more changes added

--- src/MDX/Components/Image/Image.jsx
+++
@@ -1,6 +0,0 @@
-import cn from 'classnames';
-import styles from './Image.module.scss';
-
-export default function Image({ className, ...props }) {
-  return <img className={cn(styles.container, className)} {...props} />;
-}
--- src/MDX/MDX.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { MDXRemote } from "next-mdx-remote";
-import { components } from "./Components";
-
-export default function MDX({ mdxSource }) {
-  return <MDXRemote {...mdxSource} components={components} />;
-}
